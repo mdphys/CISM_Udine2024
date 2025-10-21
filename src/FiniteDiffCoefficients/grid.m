75eaafdb79169e5cf65ee3f5924c7b7769927dc0
Michele <michele.ducceschi@unibo.it>
2025-10-21 14:03:19 +0200

    Updates

--- src/FiniteDiffCoefficients/grid.m
+++ src/FiniteDiffCoefficients/grid.m
@@ -1,36 +1,59 @@
-%---- grids 
+%==========================================================================
+%  GRID GENERATION EXAMPLES
+%--------------------------------------------------------------------------
+%  This script illustrates different types of one-dimensional grids that
+%  can be used for finite difference or finite element discretizations.
+%
+%  It generates and plots:
+%     1. Uniform grid   - evenly spaced nodes
+%     2. Smooth grid    - smoothly varying spacing
+%     3. Random grid    - irregular node distribution
+%
+%  Developed for:
+%     CISM Udine 2024 – Computational Mechanics Course
+%
+%  Author: Michele Ducceschi
+%  Institution University of Bologna 
+%  11 May 2024
+%==========================================================================
 
 clear all
 close all
+clc
 
-M  = 20 ;
-L  = 1.2 ;
+%----------------------------------------------------------------------
+% INPUT PARAMETERS
+%----------------------------------------------------------------------
+M  = 20 ;      % Number of intervals
+L  = 1.2 ;     % Domain length
 
-h = L/M ;
+h = L / M ;    % Uniform grid spacing
 
-grid1 = (0:M)*h ;
-grid2 = (L^2 - ((0:M)*h).^2)/L ;
-grid3 = [0;L*rand(18,1);L] ;
+%----------------------------------------------------------------------
+% GRID DEFINITIONS
+%----------------------------------------------------------------------
+grid1 = (0:M) * h ;                              % Uniform grid
+grid2 = (L^2 - ((0:M)*h).^2) / L ;               % Smoothly varying grid
+grid3 = [0; L*rand(M-1,1); L] ;                  % Random non-uniform grid
 
+%----------------------------------------------------------------------
+% PLOTTING
+%----------------------------------------------------------------------
+figure('Color','w')
 
 subplot(3,1,1)
-plot(grid1,0*grid1,'linestyle','none','marker','square','markerfacecolor','k','markeredgecolor','k') ;
-set(gca,TickLabelInterpreter = 'latex') ;
-title('Uniform Grid','interpreter','latex') ;
-set(gca,'YTick',[],'fontsize',14) ;
-subplot(3,1,2) 
-plot(grid2,0*grid2,'linestyle','none','marker','square','markerfacecolor','k','markeredgecolor','k') ;
-set(gca,TickLabelInterpreter = 'latex') ;
-title('Smooth Grid','interpreter','latex')
-set(gca,'YTick',[],'fontsize',14) ;
+plot(grid1,0*grid1,'ks','MarkerFaceColor','k','MarkerEdgeColor','k')
+set(gca,'TickLabelInterpreter','latex','YTick',[],'FontSize',14)
+title('Uniform Grid','Interpreter','latex')
+
+subplot(3,1,2)
+plot(grid2,0*grid2,'ks','MarkerFaceColor','k','MarkerEdgeColor','k')
+set(gca,'TickLabelInterpreter','latex','YTick',[],'FontSize',14)
+title('Smooth Grid','Interpreter','latex')
+
 subplot(3,1,3)
-plot(grid3,0*grid3,'linestyle','none','marker','square','markerfacecolor','k','markeredgecolor','k') ;
-set(gca,TickLabelInterpreter = 'latex') ;
-title('Non-Smooth Grid','interpreter','latex')
-set(gca,'YTick',[],'fontsize',14) ;
-xlabel('$x$ (m)','interpreter','latex')
+plot(grid3,0*grid3,'ks','MarkerFaceColor','k','MarkerEdgeColor','k')
+set(gca,'TickLabelInterpreter','latex','YTick',[],'FontSize',14)
+title('Random Grid','Interpreter','latex')
 
-
-
-
-
+sgtitle('1D Grid Examples','Interpreter','latex','FontSize',16)
